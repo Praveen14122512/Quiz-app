b65771822adc88e6eb4656792d96b8c7efa5a2fa
unknown <praveenanuganti601@gmail.com>
2024-05-02 17:56:15 +0530

    Add existing project files to Git

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,13 @@
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers); //shuffle prints different order
+    shuffledList.shuffle();
+
+    return shuffledList;
+  }
+}
